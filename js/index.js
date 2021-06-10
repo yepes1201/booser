9030318e81ea62e7c0f574129250951ff66be0bc
Daniel Yepes <yepes@Daniels-MacBook-Pro.local>
2021-06-09 20:07:57 -0500

    Download page finish.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     var menu = document.querySelector('#menu')
     var menuOpenBtn = document.querySelector('#open-menu')
-    
     menuOpenBtn.addEventListener('click', function() {
         console.log('toggle')
         menu.classList.toggle('deactive')
     },false)
-
     var test = setInterval(function(){
         var winValidation = window.innerWidth > 750
         if(winValidation){
@@ -14,4 +12,18 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(true);
         }
     },1000)
+
+    //Smooth Animation on document
+    var anchorTags = document.querySelectorAll('a')
+    anchorTags.forEach( aTag => {
+        if(!aTag.getAttribute('href').includes('.html')){
+            aTag.addEventListener('click', scrollAnim)
+        }
+    })
 })
+
+function scrollAnim(e){
+    e.preventDefault();
+    const href = this.getAttribute('href')
+    document.querySelector(href).scrollIntoView({behavior: 'smooth'})   
+}
